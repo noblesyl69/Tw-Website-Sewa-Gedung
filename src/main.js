9206847933e718a8868e1f084232f142359c688c
noblesyl69 <noblesyl69@gmail.com>
2024-04-07 21:16:07 +0700

    membuat bagian dashboard dan success

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+window.onscroll = function () {
+    const header = document.querySelector('#headerNav');
+    const navtop = header.offsetTop;
+
+    if (window.pageYOffset >navtop) {
+        header.classList.add("nav-Fixed");
+    }else{
+        header.classList.remove("nav-Fixed");
+    }
+}
+
+
 const navbar = document.querySelector("#navbar");
 const menuBurger = document.querySelector("#menuBurger");
 const iconFa = document.querySelector(".fa-bars");
